b320b8ae36a5fd7c404370c5e7defe0d156803f2
Satria Aji Putra <satriamaxt@gmail.com>
2019-10-22 08:37:25 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan44/hukumohm/Baterai.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan44.hukumohm;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat program berbasis object untuk menghitung tegangan
+ * dengan hukum ohm
+ */
+public class Baterai {
+    private float kuatArus, hambatan;
+
+    public Baterai() {
+        System.out.println("=====Hukum Ohm=====");
+        System.out.println("Kuat arus yang mengalir pada suatu kawat penghantar\n"
+                +"akan berbanding lurus dengan beda potensial\n"
+                +"pada ujung-ujung kawat penghantar tersebut\n"
+                +"asalkan suhu kawat dijaga konstan.\n");
+    }
+    
+    public Baterai(float kuatArus, float hambatan) {
+        this.hambatan = hambatan;
+        this.kuatArus = kuatArus;
+        
+        System.out.println("Kuat Arus : " + getKuatArus());
+        System.out.println("Hambatan : " + getHambatan());
+        System.out.println("Hasil Tegangan : " + hitungTegangan());
+        
+    }
+    
+    public float getKuatArus() {
+        return kuatArus;
+    }
+
+    public float getHambatan() {
+        return hambatan;
+    }
+    
+    public float hitungTegangan() {
+        return hambatan * kuatArus;
+    }
+    
+}
---
+++ src/pbo2/pkg10118068/latihan44/hukumohm/PBO210118068Latihan44HukumOhm.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan44.hukumohm;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat program berbasis object untuk menghitung tegangan
+ * dengan hukum ohm
+ */
+public class PBO210118068Latihan44HukumOhm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Baterai();
+        Baterai baterai = new Baterai(3, 12);
+    }
+    
+}
